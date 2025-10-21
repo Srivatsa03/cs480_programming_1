ddaa7db8f076f3fc720741a05f722e571873478a
Srivatsa03 <skamb10@uic.edu>
2025-10-21 12:35:56 -0500

    Second Task Commit

--- task2.sql
+++ task2.sql
@@ -1,4 +1,5 @@
-DROP DATABASE IF EXISTS hw_prog_1;
+Terminal close -- exit!
+hw_prog_1;
 CREATE DATABASE hw_prog_1;
 USE hw_prog_1;
 
@@ -35,20 +36,39 @@ INSERT INTO Publication VALUES
 
 
 -- Task 2.1: Total citations per venue, ordered by venue_name in ascending order.
-
+SELECT v.name AS venue_name, SUM(p.citations) AS total_citations
+FROM Venue v
+JOIN Publication p ON v.venue_id = p.venue_id
+GROUP BY v.name
+ORDER BY venue_name ASC;
 
 
 -- Task 2.2: Average citations per author, ordered by author in ascending order.
 
-
+SELECT author, AVG(citations) AS avg_citations
+FROM Publication
+GROUP BY author
+ORDER BY author ASC;
 
 -- Task 2.3: Total citations for all conference publications
 
-
+SELECT SUM(p.citations) AS conference_citations
+FROM Publication p
+JOIN Venue v ON p.venue_id = v.venue_id
+WHERE v.type = 'Conference';
 
 -- Task 2.4 Average citations per venue type (Journal vs Conference), ordered by venue type in ascending order.
 
-
+SELECT v.type, AVG(p.citations) AS avg_citations
+FROM Publication p
+JOIN Venue v ON p.venue_id = v.venue_id
+GROUP BY v.type
+ORDER BY v.type ASC;
 
 -- Task 2.5 Authors with total citations above 150, ordered by author in ascending order.
 
+SELECT author, SUM(citations) AS total_citations
+FROM Publication
+GROUP BY author
+HAVING SUM(citations) > 150
+ORDER BY author ASC;
